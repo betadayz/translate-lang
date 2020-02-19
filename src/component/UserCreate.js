04aeb66c1ec74c1d10e7f558e4f9dc3185ef42cb
Beta Dayz <betadayz888@gmail.com>
2020-02-19 11:48:22 +0100

    touch more setup

---
+++ src/component/UserCreate.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Field from './Field';
+import Button from './Button';
+
+const UserCreate = () => {
+    return (
+        <div className="ui form">
+            Jesus is lord!
+            <Field/>
+            <Button/>
+        </div>
+    );
+};
+
+export default UserCreate;
